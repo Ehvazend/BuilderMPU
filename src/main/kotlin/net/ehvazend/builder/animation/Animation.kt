b06c79c3caeea748b0e0538261526820e327ec97
Nourepide <nourepide@gmail.com>
2017-10-19 03:14:39 +0400

    Update Animation (Interpolator default fix)

--- src/main/kotlin/net/ehvazend/builder/animation/Animation.kt
+++ src/main/kotlin/net/ehvazend/builder/animation/Animation.kt
@@ -1,6 +1,7 @@
 package net.ehvazend.builder.animation
 
 import javafx.animation.Interpolator
+import javafx.animation.Interpolator.LINEAR
 import javafx.animation.KeyFrame
 import javafx.animation.KeyValue
 import javafx.animation.Timeline
@@ -9,8 +10,7 @@ import javafx.util.Duration
 
 object Animation {
     // Extension Function for Property
-    fun <T> Property<T>.timeline(toValue: T, duration: Double, cycleCount: Int = 0,
-                                 interpolator: Interpolator = Interpolator.SPLINE(1.0, 0.2, 0.2, 1.0)): Timeline {
+    fun <T> Property<T>.timeline(toValue: T, duration: Double, interpolator: Interpolator = LINEAR, cycleCount: Int = 0): Timeline {
         val keyValue = KeyValue(this, toValue, interpolator)
         val keyFrame = KeyFrame(Duration.seconds(duration), keyValue)
 
