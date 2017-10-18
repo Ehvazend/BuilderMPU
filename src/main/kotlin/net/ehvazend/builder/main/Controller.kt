fd3b5438caee3855986052e8e0232903a312969a
Nourepide <nourepide@gmail.com>
2017-10-19 03:22:02 +0400

    Upgrade panel system

--- src/main/kotlin/net/ehvazend/builder/main/Controller.kt
+++ src/main/kotlin/net/ehvazend/builder/main/Controller.kt
@@ -1,6 +1,5 @@
 package net.ehvazend.builder.main
 
-import javafx.animation.Interpolator
 import javafx.fxml.Initializable
 import javafx.scene.effect.ColorAdjust
 import javafx.scene.layout.VBox
@@ -8,19 +7,27 @@ import net.ehvazend.builder.animation.Animation.timeline
 import net.ehvazend.builder.animation.getRoot
 import java.net.URL
 import java.util.*
-import kotlin.concurrent.thread
 
 class Controller : Logic(), Initializable {
     override fun initialize(location: URL, resources: ResourceBundle?) {
+        // Fill data
+        init()
+
         // Load Init.fxml
-        root.children += getRoot<VBox>("/assets/main/Init.fxml").also { it ->
+        Data.initPanel = getRoot<VBox>("/assets/main/Init.fxml")
+
+        // Set Init.fxml
+        Data.root.children += Data.initPanel
+        Data.currentPanel = Data.initPanel
+
+        // Set effect
+        Data.initPanel.also {
+            // Effect
             it.opacityProperty().set(0.0)
-            it.opacityProperty().timeline(1.0, 1.5, interpolator = Interpolator.LINEAR)
+            it.opacityProperty().timeline(1.0, 1.5)
         }
 
-        thread {
-            // Start ColorAdjust effect on Rectangle in Root panel (Main.fxml)
-            (rootRectangle.effect as ColorAdjust).hueProperty().timeline(1.0, 40.0, -1)
-        }
+        // Set ColorAdjust effect on Rectangle in Root panel (Main.fxml)
+        (Data.rootRectangle.effect as ColorAdjust).hueProperty().timeline(1.0, 40.0, cycleCount = -1)
     }
 }
