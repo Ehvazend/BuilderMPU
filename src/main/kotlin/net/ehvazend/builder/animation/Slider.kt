615166d68739692f7fbc93c781da85a8b08b6d37
Nourepide <nourepide@gmail.com>
2017-10-21 16:04:30 +0400

    Upgrade Slider (Added stepPanel)

--- src/main/kotlin/net/ehvazend/builder/animation/Slider.kt
+++ src/main/kotlin/net/ehvazend/builder/animation/Slider.kt
@@ -1,3 +1,24 @@
 package net.ehvazend.builder.animation
 
-object Slider
+import javafx.scene.Node
+import net.ehvazend.builder.animation.Animation.translate
+
+object Slider {
+    enum class Side(val y: Double, val x: Double) {
+        TOP(x = 0.0, y = -250.0),
+        RIGHT(x = 600.0, y = 0.0),
+        BOTTOM(x = 0.0, y = 250.0),
+        LEFT(x = -600.0, y = 0.0)
+    }
+
+    fun stepPanel(currentNode: Node, newNode: Node, side: Side) {
+        // Start moving currentNode
+        currentNode.translate(side)
+
+        // Set coordinate
+        newNode.relocate(side.x, side.y)
+
+        // Start moving newNode
+        newNode.translate(side)
+    }
+}
