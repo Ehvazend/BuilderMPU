5dd588bd6b94ea4cfd8bd7c24fa913173c90a396
Nourepide <nourepide@gmail.com>
2017-11-01 02:27:50 +0400

    Move object as nested animation.Slider -> animation.Effect

--- src/main/kotlin/net/ehvazend/builder/animation/Effect.kt
+++ src/main/kotlin/net/ehvazend/builder/animation/Effect.kt
@@ -7,6 +7,26 @@ import javafx.scene.Node
 import javafx.util.Duration
 
 object Effect {
+    object Slider {
+        enum class Side(val y: Double, val x: Double) {
+            TOP(x = 0.0, y = -250.0),
+            RIGHT(x = 600.0, y = 0.0),
+            BOTTOM(x = 0.0, y = 250.0),
+            LEFT(x = -600.0, y = 0.0)
+        }
+
+        fun stepPanel(currentNode: Node, newNode: Node, side: Side) {
+            // Start moving currentNode
+            currentNode.translate(side)
+
+            // Set coordinate
+            newNode.relocate(side.x, side.y)
+
+            // Start moving newNode
+            newNode.translate(side)
+        }
+    }
+
     // Extension Function for Property
     fun <T> Property<T>.timeline(toValue: T, duration: Double, interpolator: Interpolator = LINEAR, cycleCount: Int = 0): Timeline {
         val keyValue = KeyValue(this, toValue, interpolator)
