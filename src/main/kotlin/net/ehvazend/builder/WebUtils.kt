362468b63ed7e9abd0ad8d6ab5ead54b3a53f25d
Nourepide <nourepide@gmail.com>
2018-02-04 06:05:22 +0400

    Add GameVersionData for WebUtils

--- src/main/kotlin/net/ehvazend/builder/WebUtils.kt
+++ src/main/kotlin/net/ehvazend/builder/WebUtils.kt
@@ -1,5 +1,6 @@
 package net.ehvazend.builder
 
+import com.typesafe.config.ConfigObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -19,6 +20,23 @@ object WebUtils {
             return@run ""
         }
     }!!
+
+    data class GameVersionData(val fileType: FileType, val gameVersion: String, val projectFileID: Int) {
+        companion object {
+            fun fill(configObject: ConfigObject) = GameVersionData(
+                when (configObject["FileType"]?.atKey("FileType")?.getString("FileType")) {
+                    "alpha" -> FileType.ALPHA
+                    "beta" -> FileType.BETA
+                    "release" -> FileType.RELEASE
+
+                    else -> throw IllegalArgumentException("Unidentified type of FileType")
+                },
+                configObject["GameVesion"]?.atKey("GameVersion")?.getString("GameVersion")!!,
+                configObject["ProjectFileID"]?.atKey("ProjectFileID")?.getInt("ProjectFileID")!!
+            )
+
+        }
+    }
 
     enum class FileType {
         ALPHA,
